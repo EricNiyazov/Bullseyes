07107c9197a1a75b64641c54daddebd2d3087993
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-03-15 23:41:24 -0400

    added a another for loop that would make random bullseyes pop up and it would ask you what color you want and what size you want

--- bullsEyes.py
+++ bullsEyes.py
@@ -9,6 +9,7 @@ def draw_ci (cX, cY, radius, color, win):
 
 ans1 = False
 ans2 = False
+ans3 = False
 
 while ans1 == False:
     try:
@@ -16,8 +17,6 @@ while ans1 == False:
         ans1 = True
     except ValueError:
         print ("Value Error try again use the example")
-
-
         
 while ans2 == False:
     try:
@@ -25,9 +24,18 @@ while ans2 == False:
         ans2 = True
     except ValueError:
         print ("Value Error try again use the example")
+        
+while ans3 == False:
+    try:
+        ciSz = int(input("what size do you want please enter a numerical integer"))
+        ans3 = True
+    except ValueError:
+        print ("Value Error try again use numerical integer")
 
-bullWin = GraphWin("bullsEyes.py", 500, 500)
-bullWin.setCoords(0, 0, 500, 500)
+
+
+bullWin = GraphWin("bullsEyes.py", ciSz*10, ciSz*10)
+bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
 def draw_be(bX, bY, rings, rSize, bColor1, bColor2, bWin):
     bSize = rings * rSize
@@ -38,7 +46,12 @@ def draw_be(bX, bY, rings, rSize, bColor1, bColor2, bWin):
             cCol = bColor2
         draw_ci(bX, bY, bSize, cCol, bWin)
         bSize -= rSize
+                   
+for j in range (20):
+    radX = randint(ciSz, ciSz * 10)
+    radY = randint(ciSz, ciSz * 10)
+    ranSiz = randint(1, 25)
+    draw_be(radX, radY, 10, 4, rngCol1 , rngCol2, bullWin)
 
-
-draw_be(250, 250, 10, 10, rngCol1, rngCol2, bullWin)
-
+bullWin.getMouse()
+bullWin.close()
