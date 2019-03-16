45f7ee825e6dcb524d25aeea08a23ceb091adc34
Eric Niyazov <ERIC@Margaritas-iMac.home>
2019-03-15 23:09:30 -0400

    A custom bull's eye function calls the custom circle function to create a bull's eye in the window.

--- bullsEyes.py
+++ bullsEyes.py
@@ -9,5 +9,16 @@ def draw_ci (cX, cY, radius, color, win):
 bullWin = GraphWin("bullsEyes.py", 500, 500)
 bullWin.setCoords(0, 0, 500, 500)
 
-draw_ci (250, 250, 50, "black", bullWin)
+def draw_be(bX, bY, rings, rSize, bColor1, bColor2, bWin):
+    bSize = rings * rSize
+    for i in range (rings):
+        if (i) % 2 == 1:
+            cCol = bColor1
+        else:
+            cCol = bColor2
+        draw_ci(bX, bY, bSize, cCol, bWin)
+        bSize -= rSize
+
+        
+draw_be(250, 250, 10, 10, "black", "red", bullWin)
 
